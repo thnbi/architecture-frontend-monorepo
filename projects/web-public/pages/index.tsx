96dec1b7264bf4579c3e0e968b80f4853c06322e
thnbi <renato_freitas13@icloud.com>
2023-01-06 01:52:04 -0300

    reutilização do tsconfig e configuração correta do styled components

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,11 +1,13 @@
-import { sum } from '@thnbi/utils/math/sum';
-import { Text } from '@thnbi/design-system/components/Text';
+import { sum } from "@thnbi/utils/math/sum";
+import { Text } from "@thnbi/design-system/components/Text";
 
 export default function HomeScreen() {
   return (
     <main>
-      <Text tag='h1'>Home</Text>
-      <Text tag='p'>módulo local: @thnbi/utils/math/sum "sum(2,2)" = {sum(2,2)}</Text>
+      <Text tag="h1">Home</Text>
+      <Text tag="p">
+        módulo local: @thnbi/utils/math/sum "sum(2,2)" = {sum(2, 2)}
+      </Text>
     </main>
   );
 }
